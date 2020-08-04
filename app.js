a2234526333a8eea5dad9e49eb7c37f4cd5a387a
Amanjot Singh <ammysingh1999@gmail.com>
2020-08-04 20:36:00 +0530

    ...

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ btn.addEventListener('click',(e)=>{
     element.scrollIntoView();
 });
 
+$(document).Ready(function(){
+   
+    $('#carouselButton').click(function(){
+        if ($('#carouselButton').children('span').hasClass("fa-pause")) {
+            $('#carouselslides').carousel('pause');
+            $('#carouselButton').children('span').removeClass("fa-pause");
+            $('#carouselButton').children('span').addClass("fa-play");
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')){
+            $('#carouselslides').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');                    
+        }
+    });
+});
+   
